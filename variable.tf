9dc449d3da34ce52ab3f5d00267aec0b9a7818f2
Rohan Shahi <64183297+rohanlucifer@users.noreply.github.com>
2023-04-03 18:26:19 +0545

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,22 @@
+variable "db_parameter_group_name" {
+  type = string
+  #value = "strongsecurestring"
+  default = ""
+}
+
+#variable "db_family" {
+ # type = string
+  #value = "strongsecurestring"
+  #default = ""
+#}
+variable "db_username" {
+  type = string
+  #value = "strongsecurestring"
+  default = ""
+}
+
+variable "db_password" {
+  type = string
+  #value = "strongsecurestring"
+  default = ""
+}
